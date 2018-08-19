cd0262d8b1614117b6e41cb567950178c7eeb26d
Mohamed Hesham Alma'zon <m.hesham@outlook.com>
2018-08-19 07:29:26 +0200

    Add files via upload

---
+++ app/routes/cart.js
@@ -0,0 +1,9 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+  shoppingCart: Ember.inject.service('shopping-cart'),
+
+  model() {
+    return this.get('shoppingCart').getCart();
+  }
+});
---
+++ app/services/shopping-cart.js
@@ -0,0 +1,14 @@
+import Ember from 'ember';
+
+export default Ember.Service.extend({
+  cart: [],
+
+  addToCart(color, size, price) {
+    this.get('cart').push( { color: color, size: size, price: price } );
+  },
+
+  getCart() {
+    return this.get('cart');
+  }
+
+});
